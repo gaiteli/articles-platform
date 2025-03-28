5f6f7c483811a4dab81ee99d603642e1f700b249
Jay <lijj2024@mail.ustc.edu.cn>
2025-03-28 23:07:25 +0800

    补充README.md文件

--- server/utils/aliyun.js
+++ server/utils/aliyun.js
@@ -16,18 +16,21 @@ const client = new OSS(config)
 // multer配置信息
 const getUploadMiddleware = (folder = 'uploads', maxSize = 2) => {
     return multer({
+        // 存储配置
         storage: MAO({
             config: config,
             destination: `${folder}`.replace(/\/$/, '') // 自动处理路径斜杠
         }),
+        // 文件大小限制
         limits: {
             fileSize: maxSize * 1024 * 1024
         },
+        // 文件类型过滤
         fileFilter: function(req, file, cb) {
             const isImage = file.mimetype.split('/')[0] === 'image';
             isImage ? cb(null, true) : cb(new BadRequest('只允许上传图片'));
         }
-    }).single('image'); // 保持字段名一致
+    }).single('image'); // 单文件上传，注意保持字段名一致
 };
 
 module.exports = {
